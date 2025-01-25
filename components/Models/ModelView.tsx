29fd25bcd405b4e64c34b27e6b64625f1dbbd1b7
thief <thiefainedev@gmail.com>
2025-01-25 14:51:18 +0100

    fix(light): reduce light

--- components/Models/ModelView.tsx
+++ components/Models/ModelView.tsx
@@ -6,7 +6,7 @@ import { Model3D } from '@/components/Models/Model3D';
 export const ModelView = () => {
   return (
       <Canvas camera={{ position: [2, 2, -0.5], fov: 60 }}>
-        <ambientLight intensity={2} />
+        <ambientLight intensity={1} />
         <directionalLight position={[1, 1, 0]} intensity={1} />
         <Model3D />
         <OrbitControls minDistance={3} maxDistance={4.5} enablePan={false} />
